c26cc04b93857885d55c3082f1eec4988d659d2f
Chirag <33632821+Chiraaag18@users.noreply.github.com>
2020-09-10 19:33:02 +0530

    Files Committed: footer.php, front-page.php, functions.php, header.php

---
+++ front-page.php
@@ -0,0 +1,111 @@
+<?php get_header();?>
+
+<div class="col-container">
+  <div class="col" style="background:white">
+    <h2>Some Texts</h2>
+    <button class="button">Button</button>
+    
+  </div>
+
+  <div class="col" style="background:#F39C12">
+    <h2>Column 2</h2>
+    
+  </div>
+
+ 
+</div>
+<div style="margin-top: 15px">
+  <div class="transbox">
+  <button class="button", style="margin-left: 30px">Button</button>
+  <button class="button", style="margin-left: 15px">Button</button>
+  <button class="button", style="margin-left: 15px">Button</button>
+  <button class="button", style="margin-left: 15px">Button</button>
+  <button class="button", style="margin-left: 15px">Button</button>
+  <button class="button ", style="float: right; margin-right:70px">Button</button>    
+  </div>
+</div>
+
+<div style="margin-top: 20px; margin-left: 100px">
+
+  <div class="col" style="background:#F12828">
+    <h2>Some Texts</h2>
+    
+    
+  </div>
+
+  <div class="col" style="background:#EC6E6E; margin-left: 150px">
+    <h2>Column 2</h2>
+    
+  </div>  
+
+ 
+</div>
+
+<div class="box", style="margin-top: 40px">
+  <div class="inside_box", style="margin-top: 15px; margin-bottom: 15px; margin-left: 150px; margin-right: 15px; float: left">
+    <h2>Some Texts</h2>
+  </div>
+
+  <div class="inside_box1", style="margin-top: 15px; margin-bottom: 15px; margin-left: 1000px; margin-right: 15px">
+    <h2>Some Texts</h2>
+    <button class="button", style="margin-left: 400px; margin-top: 150px; background: black; color: white "><h1><b>></b></h1></button>
+  </div>
+ 
+
+</div>
+
+<div class="box1", style="margin-top: 40px">
+  <div class="inside_boxx", style="margin-top: 15px; margin-bottom: 15px; margin-left: 150px; margin-right: 15px; float: left">
+    <h2>Some Texts</h2>
+  </div>
+
+  <div class="inside_boxx1", style="margin-top: 15px; margin-bottom: 15px; margin-left: 1000px; margin-right: 15px">
+    <h2>Some Texts</h2>
+  </div>
+ 
+
+</div>
+
+<div class="box", style="margin-top: 40px">
+  <div class="inside_box", style="margin-top: 15px; margin-bottom: 15px; margin-left: 150px; margin-right: 15px; float: left">
+    <h2>Some Texts</h2>
+  </div>
+
+  <div class="inside_box1", style="margin-top: 15px; margin-bottom: 15px; margin-left: 1000px; margin-right: 15px">
+    <h2>Some Texts</h2>
+  </div>
+ 
+
+</div>
+
+<div class="box1", style="margin-top: 40px">
+  <div class="inside_boxx", style="margin-top: 15px; margin-bottom: 15px; margin-left: 150px; margin-right: 15px; float: left">
+    <h2>Some Texts</h2>
+  </div>
+
+  <div class="inside_boxx1", style="margin-top: 15px; margin-bottom: 15px; margin-left: 1000px; margin-right: 15px">
+    <h2>Some Texts</h2>
+  </div>
+ 
+
+</div>
+
+<div style="background:white; color:black; margin-top: 15px; height: 500px; width:100%">
+<div class="coll" style="background:white">
+    <h2>Some Texts</h2>
+    
+    
+  </div>
+
+  <div class="coll" style="background:white; margin-left: 120px">
+    <h2>Column</h2>
+    
+  </div>
+  <div class="coll" style="background:white; margin-left: 120px">
+    <h2>Column</h2>
+    
+  </div>
+</div>
+
+
+<?php get_footer();?>
---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php wp_head();?>
+    </head>
+<body <?php body_class();?>>
+
+
+<header class="sticky-top">
+<div style="clear: both">
+    <div>
+    <button class="w3-button w3-xlarge w3-circle w3-black", style="float: right; margin-right:15px; background: #BA4A00; border-color: #BA4A00">x</button>
+    <h2 style="float: top">Some Random Text</h2>
+    
+    </div>    
+    
+    <h3 style="float: bottom"><button class="button ", style="float: right; margin-right:50px; background: black; color: white">Login</button></h3>
+</div>
+
+</header>
+
+<div style="padding top: 800px;">
+    <h1 style="float: left">Some Random Text</h1>
+    
+    <h1 style="float: right">OLA</h1>
+</div>
